be82efd49cb02ee81e7472b143d3b7dca4fbd046
Guilherme Fernando Portela de Oliveira <gfernandoportelaoliveira@gmail.com>
2026-09-06 18:15:49 -0300

    Add Prisma 7.8.0 e Category.repository.js feito

---
+++ api/src/repositories/category.repository.js
@@ -0,0 +1,18 @@
+const Category = [
+  {
+    id: 1,
+    name: "Infra",
+  },
+  {
+    id: 2,
+    name: "Desenvolvimento",
+  },
+  {
+    id: 3,
+    name: "Design",
+  },
+  {
+    id: 4,
+    name: "Planejamento",
+  },
+];
